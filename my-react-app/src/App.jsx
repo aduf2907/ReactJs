e297e8d728f9e89fa8ab7ef4559db0321e67ac85
aduf <dufalmira@gmail.com>
2024-03-05 20:17:59 +0700

    buat halaman login sebelum menggunakan atomic design

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,50 +1,42 @@
-/* eslint-disable react/prop-types */
-// import React from "react";
-
-// class Button extends React.Component {
-//   render() {
-//     return (
-//       <button
-//         className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white"
-//         type="submit"
-//       >
-//         Buy now
-//       </button>
-//     );
-//   }
-// }
-
-// ini yg direkomendari, soalnya masih ada di dokumentasi terbaru
-// function ButtonBlack() {
-//   return (
-//     <button
-//       className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-//       type="submit"
-//     >
-//       Buy now
-//     </button>
-//   );
-// }
-
-const Button = (props) => {
-  const { children = "...", variant = "bg-black" } = props; //bisa nambahin nilai defaultnya juga
-  return (
-    <button
-      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
-      type="submit"
-    >
-      {children}
-    </button>
-  );
-};
+import Button from "./components/elements/Button";
 
 function App() {
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div className="flex gap-x-2">
-        <Button variant="bg-red-700">Login</Button>
-        <Button variant="bg-slate-700">Sign-Up</Button>
-        <Button variant="bg-black">Logout</Button>
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="w-full max-w-xs">
+        <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
+        <p className="font-medium text-slate-500 mb-8">
+          Welcome, please enter your details
+        </p>
+        <form action="">
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="block text-slate-700 text-sm font-bold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50"
+              placeholder="example@gmail.com"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-slate-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50"
+              placeholder="********"
+            />
+          </div>
+          <Button classname="bg-blue-600 w-full">Login</Button>
+        </form>
       </div>
     </div>
   );
