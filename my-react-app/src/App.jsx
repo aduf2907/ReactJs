f13b4711d094f2edfe1b0b95a5e5fec020d90126
aduf <dufalmira@gmail.com>
2024-03-05 16:11:57 +0700

    component & props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,51 @@
+/* eslint-disable react/prop-types */
+// import React from "react";
+
+// class Button extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white"
+//         type="submit"
+//       >
+//         Buy now
+//       </button>
+//     );
+//   }
+// }
+
+// ini yg direkomendari, soalnya masih ada di dokumentasi terbaru
+// function ButtonBlack() {
+//   return (
+//     <button
+//       className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+//       type="submit"
+//     >
+//       Buy now
+//     </button>
+//   );
+// }
+
+const Button = (props) => {
+  const { children = "...", variant = "bg-black" } = props; //bisa nambahin nilai defaultnya juga
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div>Hello Wordl!</div>
+      <div className="flex gap-x-2">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Sign-Up</Button>
+        <Button variant="bg-black">Logout</Button>
+      </div>
     </div>
   );
 }
