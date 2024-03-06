2a3d4b3ad4a27230dfc11c6d89e6f0aab9237798
aduf <dufalmira@gmail.com>
2024-03-06 21:33:08 +0700

    implementasi react-router dan menambahkan id di inputan biar label di klik langsung aktif inputannya

--- my-react-app/src/pages/login.jsx
+++ my-react-app/src/pages/login.jsx
@@ -1,10 +1,17 @@
 import AuthLayout from "../components/layout/AuthLayouts";
 import FormLogin from "../components/fragments/FormLogin";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
     <AuthLayout title="Login">
       <FormLogin></FormLogin>
+      <p className="text-sm mt-5 text-center">
+        Don't have an account?{" "}
+        <Link to="/register" className="font-bold text-blue-600">
+          Sign Up
+        </Link>
+      </p>
     </AuthLayout>
   );
 };
--- my-react-app/src/pages/register.jsx
+++ my-react-app/src/pages/register.jsx
@@ -1,10 +1,17 @@
 import AuthLayout from "../components/layout/AuthLayouts";
 import FormRegister from "../components/fragments/FormRegister";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
     <AuthLayout title="Register">
-      <FormRegister></FormRegister>
+      <FormRegister />
+      <p className="text-sm mt-5 text-center">
+        Have an account?{" "}
+        <Link to="/login" className="font-bold text-blue-600">
+          Login
+        </Link>
+      </p>
     </AuthLayout>
   );
 };
